71287bacd7b92b6133372df6fe6512f6d9fa99a1
AKRA <timur.akra@gmail.com>
2023-09-01 16:19:06 +0300

    add new solution

---
+++ CsharpProjects/Panagram/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Panagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pangram = "The quick brown fox jumps over the lazy dog";
+
+            var arrOfStr = pangram.Split(' ').ToArray();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
