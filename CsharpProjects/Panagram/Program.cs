9cf8d45eb63df48662e3b18ec0dc2998ddfe3dc2
AKRA <timur.akra@gmail.com>
2023-09-01 16:19:47 +0300

    done

--- CsharpProjects/Panagram/Program.cs
+++ CsharpProjects/Panagram/Program.cs
@@ -8,7 +8,25 @@
 
             var arrOfStr = pangram.Split(' ').ToArray();
 
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < arrOfStr.Length; i++)
+            {
+                var localArr = arrOfStr[i].ToCharArray();
+
+                Array.Reverse(localArr);
+
+                arrOfStr[i] = string.Join("", localArr);
+            }
+
+            var res = "ehT kciuq nworb xof spmuj revo eht yzal god";
+
+            Console.WriteLine($"Should be like this: {res}");
+
+            var myRes = string.Join(" ", arrOfStr);
+            Console.WriteLine(myRes);
+
+            Console.WriteLine();
+
+            Console.WriteLine(res == myRes ? "DONE" : "TRY AGAIN");
         }
     }
 }
