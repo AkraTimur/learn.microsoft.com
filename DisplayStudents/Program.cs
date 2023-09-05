d3255638d1c42cf4d7d49b3e2f80009386553672
AKRA <AkraTimur@users.noreply.github.com>
2023-09-05 13:36:06 +0300

    add new solution

---
+++ DisplayStudents/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+
+PrintArray(array);
+Clear(array);
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    foreach (int a in array)
+    {
+        Console.Write($"{a} ");
+    }
+}
+
+void Clear(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = 0;
+    }
+}
